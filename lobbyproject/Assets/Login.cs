63a996ca84c5dcac120d7d0e9d254116944b2549
Ezra <ez9002@gmail.com>
2020-11-15 14:11:23 -0600

    UI Tweaks and basic input functionality
    - Added Create account button
    - Adjusted UI
    - Added basic script functionality to read the input from the player
    
    The typed input is cast to the login script but not currently used in any capacity.
    
    Next steps would be taking the player to the relevant canvas screen after the player logs in/creates an account, or requesting the player re-enter the info if the login request is invalid or the account name is taken.

---
+++ lobbyproject/Assets/Login.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Login : MonoBehaviour
+{
+    // Gets input from assigned UI elements (both input boxes and both buttons)
+    public TMP_InputField UsernameInput;
+    public TMP_InputField PasswordInput;
+    public Button LoginButton;
+    public Button CreateAccButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+          // This script doesn't do anything yet! Needs to check the account of the player and/or take them to the next page
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ lobbyproject/Assets/Web.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class Web : MonoBehaviour
+{
+    // Reading from firebase/sql server / whatever we figure out goes here
+    void Start()
+    {
+        StartCoroutine(Upload());
+    }
+
+    IEnumerator Upload()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("myField", "myData");
+
+        using (UnityWebRequest www = UnityWebRequest.Post("http://www.my-server.com/myform", form))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.isNetworkError || www.isHttpError)
+            {
+                Debug.Log(www.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+        }
+    }
+}
